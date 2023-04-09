850736dc5662cd1ab7b1e31031a388b72e2d79b7
diegoleonb <diegoleonb10@gmail.com>
2023-04-08 19:14:25 -0500

    Terminado el hook personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,33 @@
 import { Header } from "./components/Header"
 import { TaskList } from "./components/TaskList";
-import { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
+import { useTask } from "./hooks/useTask";
 
 function App() {
+
+  const [tasks, addTask, editTask, deleteTask, taskDone] = useTask();
   
-  const [tasks, setTasks] = useState(localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [
-    {name :"Trabajo - Finalizar", id: 1, descripcion : "Terminar el proyecto", done : false},
-    {name :"Trabajo - Arreglar", id: 2, descripcion : "Arreglar los bugs de la aplicacion", done : false},
-    {name :"Universidad - Aprender", id: 3, descripcion : "Aprender React", done : false},
-    {name :"Universidad - Laboratorio", id: 4, descripcion : "Terminar el laboratorio de React", done : false},
-  ]);
-
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }, [tasks]);
-
-  const handleSaveTasks = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  };
-
   const handleTaskClick = (taskId) => {
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === taskId) return {...task, done: !task.done};
-        return task;
-      })
-    );
+    taskDone(taskId);
   };
 
   const handleTaskDelete = (taskId) => {
-    setTasks(
-      tasks.filter((task) => task.id !== taskId)
-    );
+    deleteTask(taskId);
   };
 
   const handleTaskEdit = (taskId, text) => {
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === taskId) return {...task, name: text};
-        return task;
-      })
-    );
+    editTask(taskId, text);
   };
 
-  const newTask = () => {
-    const text = prompt('Type new task');
-    const descripcion = prompt('Type new task description');
-    if(text !== null && text.length > 0 && descripcion !== null && descripcion.length > 0){
-      setTasks(
-        tasks.concat({name: text, id: tasks.length + 1, descripcion: descripcion, done: false})
-      );
-    }
-    else{
-      alert("Task cannot be empty");
-    }
+  const handleTaskNew = () => {
+    addTask();
   };
 
   return (
     <div className="App">
       <Header title="List Tasks"/>
       <div className="new_task">
-        <Button size="medium" startIcon= '➕' onClick={() => newTask()}>
+        <Button size="medium" startIcon= '➕' onClick={() => handleTaskNew()}>
           Add new task
         </Button>
       </div>
@@ -69,5 +35,4 @@ function App() {
     </div>
   )
 }
-
 export default App
---
+++ src/hooks/useTask.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+
+export function useTask(){
+
+    const [tasks, setTasks] = useState(localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [
+        {name :"Trabajo - Finalizar", id: 1, descripcion : "Terminar el proyecto", done : false},
+        {name :"Trabajo - Arreglar", id: 2, descripcion : "Arreglar los bugs de la aplicacion", done : false},
+        {name :"Universidad - Aprender", id: 3, descripcion : "Aprender React", done : false},
+        {name :"Universidad - Laboratorio", id: 4, descripcion : "Terminar el laboratorio de React", done : false},
+      ]);
+
+    useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+
+    function addTask(){
+        const text = prompt('Type new task');
+        const descripcion = prompt('Type new task description');
+        if(text !== null && text.length > 0 && descripcion !== null && descripcion.length > 0){
+        setTasks(
+            tasks.concat({name: text, id: tasks.length + 1, descripcion: descripcion, done: false})
+        );
+        }
+        else{
+            alert("Task cannot be empty");
+        }
+    }
+
+    function editTask(taskId, text){
+        setTasks(tasks.map((task) => {
+            if (task.id === taskId) return {...task, name: text};
+                return task;
+            })
+        );
+    }
+
+    function deleteTask(taskId){
+        setTasks(
+            tasks.filter((task) => task.id !== taskId)
+        );
+    }
+
+    function taskDone(taskId){
+        setTasks(
+            tasks.map((task) => {
+            if (task.id === taskId) return {...task, done: !task.done};
+              return task;
+            })
+        );
+    }
+
+    return [tasks, addTask, editTask, deleteTask, taskDone];
+}
